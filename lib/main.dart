a90d0478014e56f74a50ae34a212af046a5b31d0
Akshay <akshayyadav7613@gmail.com>
2019-02-10 17:55:47 +0530

    now this the real development... hot reload is awsome..

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
 import 'package:flutter/material.dart';
+
+
+
+
+void main(){
+ runApp(new MyApp()); 
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title:"this app",
+      home: new Scaffold(
+        
+        appBar: new AppBar(
+          title: new Text("colorful math"),
+        ),
+        body: new Center(child: new Text("now begin flutter now this somthing very cool first time sing in develpment"
+            "so good "),),
+      ),
+    );
+  }
+}
