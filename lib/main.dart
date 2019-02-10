47eaf955511407ddb0f557e1d31dc606742d561a
Akshay <akshayyadav7613@gmail.com>
2019-02-10 18:01:12 +0530

    useing stateless widgets...

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,8 @@ void main(){
 
 class MyApp extends StatelessWidget {
   @override
+  
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title:"this app",
-      home: new Scaffold(
-        
-        appBar: new AppBar(
-          title: new Text("colorful math"),
-        ),
-        body: new Center(child: new Text("now begin flutter now this somthing very cool first time sing in develpment"
-            "so good "),),
-      ),
-    );
+    return Container();
   }
 }
