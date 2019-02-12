9516b9fcce468a81466429c9b58a1091590c4e95
Akshay <akshayyadav7613@gmail.com>
2019-02-12 20:24:45 +0530

    add image in pubspac.yaml and used in code...

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp() );//fat arrow notation
+
 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("appbar"),),
+       appBar: AppBar(title: Text("appbar"), elevation: 15, actions: <Widget>[],),
+     // bottomNavigationBar: BottomAppBar(elevation: 20,notchMargin: 22,color: Colors.green,),
+      body: Card(child: Column(children: <Widget>[Image.asset("assets/tac.jpg"),Text("food ")],),),
 
     ),);
   }
