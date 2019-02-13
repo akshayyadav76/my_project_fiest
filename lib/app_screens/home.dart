92272894f3296d5822442d7c2800c9481cc59798
Akshay <akshayyadav7613@gmail.com>
2019-02-13 20:58:24 +0530

    working wtih row and...

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,12 +6,24 @@ class HomePage extends StatelessWidget {
     return Center(child:   Container(
       alignment: Alignment.center,
       color: Colors.cyan,
-      child: Text("flight", textDirection: TextDirection.ltr,
-        style: TextStyle(decoration: TextDecoration.none,
-            fontSize: 33 , fontFamily: "Raleway",
-            fontWeight: FontWeight.w300),
+      child: Row(
+        children: <Widget>[
+          Text("flight", textDirection: TextDirection.ltr,
+            style: TextStyle(decoration: TextDecoration.none,
+                fontSize: 33 , fontFamily: "Raleway",
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.italic),
 
-      ),
+          ),
+           Expanded(child:  Text(" this what  we wont to do with this ", textDirection: TextDirection.ltr,
+            style: TextStyle(decoration: TextDecoration.none,
+                fontSize: 33 , fontFamily: "Raleway",
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.italic),
+          ),),
+        ],
+      )
+
      // width: 200.0,height: 200.0,
     //  margin: EdgeInsets.only(right: 40),
 
