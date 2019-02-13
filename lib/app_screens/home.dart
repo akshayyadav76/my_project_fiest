4cd0178f6f79154898c7b8c878a0a619975e94fa
Akshay <akshayyadav7613@gmail.com>
2019-02-13 21:10:50 +0530

    working with column...

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,23 +6,44 @@ class HomePage extends StatelessWidget {
     return Center(child:   Container(
       alignment: Alignment.center,
       color: Colors.cyan,
-      child: Row(
-        children: <Widget>[
-          Text("flight", textDirection: TextDirection.ltr,
-            style: TextStyle(decoration: TextDecoration.none,
-                fontSize: 33 , fontFamily: "Raleway",
-                fontWeight: FontWeight.w700,
-                fontStyle: FontStyle.italic),
+      child: Column(children: <Widget>[
 
-          ),
-           Expanded(child:  Text(" this what  we wont to do with this ", textDirection: TextDirection.ltr,
-            style: TextStyle(decoration: TextDecoration.none,
-                fontSize: 33 , fontFamily: "Raleway",
-                fontWeight: FontWeight.w700,
-                fontStyle: FontStyle.italic),
-          ),),
-        ],
-      )
+        Row(
+          children: <Widget>[
+            Expanded(child:  Text("flight", textDirection: TextDirection.ltr,
+              style: TextStyle(decoration: TextDecoration.none,
+                  fontSize: 33 , fontFamily: "Raleway",
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+
+            ),),
+            Expanded(child:  Text(" this what  we wont to do with this ", textDirection: TextDirection.ltr,
+              style: TextStyle(decoration: TextDecoration.none,
+                  fontSize: 33 , fontFamily: "Raleway",
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+            ),),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(child:  Text("flight", textDirection: TextDirection.ltr,
+              style: TextStyle(decoration: TextDecoration.none,
+                  fontSize: 33 , fontFamily: "Raleway",
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+
+            ),),
+            Expanded(child:  Text(" this what  we wont to do with this ", textDirection: TextDirection.ltr,
+              style: TextStyle(decoration: TextDecoration.none,
+                  fontSize: 33 , fontFamily: "Raleway",
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic),
+            ),),
+          ],
+        ),
+
+      ],),
 
      // width: 200.0,height: 200.0,
     //  margin: EdgeInsets.only(right: 40),
