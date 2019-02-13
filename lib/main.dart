e66871509507f650020c98357319490af99294ec
Akshay <akshayyadav7613@gmail.com>
2019-02-13 22:40:45 +0530

    used the list view.....

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,35 @@ import 'package:my_project_fiest/app_screens/home.dart';
 void main(){
   runApp(MaterialApp(
     title: "this",
-
-    home: HomePage(),
+    home: Scaffold(
+      body: getlist(),
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
+
+
+  Widget  getlist(){
+  var listview = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.accessibility,color: Colors.red,),
+
+        title: Text("first tile list"),
+        subtitle: Text("sub title "),
+        trailing: Icon(Icons.access_alarms),
+          onTap: (){
+          debugPrint("click");},
+      ),
+      ListTile(
+        leading: Icon(Icons.map,color: Colors.red,),
+        title: Text("first tile list"),
+        trailing: Icon(Icons.access_alarms),
+      ),
+      Text("this the text",),
+      Padding(padding: EdgeInsets.only(top: 44),),
+      Container( padding: EdgeInsets.only(top: 55), width: 30,height: 44,color: Colors.green,)
+    ],
+  );
+  return listview;
+    }
